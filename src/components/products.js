c62e39507549d3d4efafebf0ca23ce06ec41ed62
Aparna UdayKumar <aparnaudaykumar@Aparnas-MacBook-Air.local>
2022-05-11 16:58:22 +0530

    feat: files added

---
+++ src/components/products.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-useless-constructor */
+import React from 'react';
+
+class Products extends React.Component {
+
+    constructor() {
+      super();
+    }
+    render() {
+        return (
+            <div>
+                <h1>Products</h1>
+            </div>
+        )
+    }
+}
+
+export default Products
